4435ba8733803d8298b932fd043a494eaf0e5462
Alzawbari Mohammed <38676494+AlzawbariMoes@users.noreply.github.com>
2020-04-24 05:00:00 +0300

    here is the game with its scripts

---
+++ Assets/Script/drag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drag : MonoBehaviour {
+
+    Vector3 distance;
+    float posX;
+    float posY;
+
+    private void OnMouseDown()
+    {
+        distance = Camera.main.WorldToScreenPoint(transform.position);
+        // calculate the X position of the mouse
+        posX = Input.mousePosition.x - distance.x;
+        // calculate the Y position of the mouse
+        posY = Input.mousePosition.y - distance.y;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, distance.z);
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+        transform.position = worldPos;
+    }
+}
---
+++ Assets/Script/namePlayers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class namePlayers : MonoBehaviour
+{
+    public string playerName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        playerName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Welcome" + playerName; 
+    }
+
+}
